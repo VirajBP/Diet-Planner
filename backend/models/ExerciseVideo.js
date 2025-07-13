2e072fd1d4a70adb77b6f7d66c07f0f278a6f0cb
Viraj Bipin Pradhan <134828338+VirajBP@users.noreply.github.com>
2025-07-13 16:16:10 +0530

    Schema upgradation and database changes for Exercise page data

--- backend/models/ExerciseVideo.js
+++ backend/models/ExerciseVideo.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
 
 const exerciseVideoSchema = new mongoose.Schema({
-  name: {
-    type: String,
+  id: {
+    type: mongoose.Schema.Types.ObjectId,
     required: true
   },
-  tag: {
-    type: String,
-    required: true
-  },
-  details: {
-    type: String,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-});
+  name: { type: String, required: true },
+  tag: { type: String, required: true },
+  details: { type: String, required: true },
+  link: { type: String, required: true },
+  steps: [{ type: String, required: true }],
+  calories: { type: Number, required: true },
+  duration: { type: String, required: true },
+  difficulty: { type: String, required: true }
+}, { timestamps: true });
 
 module.exports = mongoose.model('ExerciseVideo', exerciseVideoSchema); 
